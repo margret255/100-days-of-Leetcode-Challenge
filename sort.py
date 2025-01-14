249df20a0a9db010212c9cc85f9a4ced49643d5b
Wangui Margaret <148498603+margret255@users.noreply.github.com>
2025-01-14 14:26:32 +0300

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,30 +1,21 @@
-#Function to capture the student details
-def Student_details():
-    students=[]
-    no_of_students=int(input("Enter the number of students"))
-   
-    for _ in range(no_of_students):
-     student={}
-     student['name']=input("Enter the students name")
-     student['Reg No']=input("Enter the student registration number")
-     student['marks']=input("Enter the students marks")
-     students.append(student)
-     return students
-    #Function to sort students based on the marks in ascending order
-    def sort_students_by_marks(students):
-       return sorted(students,key=lambda student:student['marks'])
-    
-    #main fuction to cordinate process of capturing and sorting student details
+Class Student:
+def__init__(self,name,program):
+    self.name=name
+    self.program=program
 
-    def main():
-       students=Student_details()
-       sorted_students=sort_students_by_marks(students)
-
-       print("\mSorted student details by marks(ascending order):")
-       for student in sorted_students:
-          print(f"Name:{student['name']},Roll Number:{student['roll_number']},Marks:{student['marks']}")
-
-          if __name__ == "__main__":
-             main()
+Class Registration:
+    def__init__(self)
+        self.students=[]
 
 
+    def register(self,name,program)
+        student=Student(name,program)
+        self.students.append(student)
+
+
+    def sort_by_program(self)
+    self.students.sort(key=lambda s: s.program)
+
+    def dispaly(self)
+        for student in self.students:
+            print(f"name{student.name},program{student.program}")
